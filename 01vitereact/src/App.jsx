040135d7ec6f9f05a3f4c7c0e16f7ac73aec00b3
sn080 <sn08064021@gmail.com>
2025-08-19 22:56:28 +0530

    password generator

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,11 @@
 import Demo from "./demo"
 
 function App() {
+  const username = "chai do" 
   return (
     <>
     <Demo/>
-    <h1>GGGG</h1>
+    <h1>GGGG {username}</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, animi.</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,37 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//       type: 'a',
+//       props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//       },
+//       children: 'Click me to visit Google'
+//     }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target:'_blank'},
+  'click me to visit google'
+)
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    reactElement
   </StrictMode>,
 )
